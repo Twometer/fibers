61519640008ad9320bf4ff48b48cab8ce040de82
Twometer <twometer@outlook.de>
2021-02-07 12:15:33 +0100

    Add test subscriber app

---
+++ test/subscriber.js
@@ -0,0 +1,19 @@
+const WebSocket = require('ws');
+
+var socket = new WebSocket('ws://localhost:8090/test-fiber/subscribe', {
+    headers: {
+        Authorization: "X-FiberAuth cvhYgf4yhCWRL59z639x"
+    }
+});
+
+socket.on('open', () => {
+    console.log("Connected");
+});
+
+socket.on('close', (e) => {
+    console.log("Connection lost", e);
+});
+
+socket.on('message', (data) => {
+    console.log(data);
+});
